9d2f741615a01430931de4494b0a20d9301e4fd7
GZTan23 <guan_zhong@GuanZhong.>
2025-01-24 03:13:31 +0800

    Changed Duke to Bane

---
+++ src/main/java/Bane.java
@@ -0,0 +1,8 @@
+public class Bane {
+    public static void main(String[] args) {
+      System.out.println("Hello, it is me, Bane.");
+      System.out.println("What do you want from me?\n");
+      
+      System.out.println("Bye, hope to not see you again.");
+    }
+}
