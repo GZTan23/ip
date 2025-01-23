1f615802b6aed69d567ba0e7e8d21248223d0a00
GZTan23 <guan_zhong@GuanZhong.>
2025-01-24 03:35:00 +0800

    Added echo functionality to Bane

--- src/main/java/Bane.java
+++ src/main/java/Bane.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Bane {
     public static void main(String[] args) {
-      System.out.println("Hello, it is me, Bane.");
-      System.out.println("What do you want from me?\n");
+    Scanner sc = new Scanner(System.in);
       
-      System.out.println("Bye, hope to not see you again.");
+    System.out.println("___________________________________________________\n");
+    System.out.println("Hello, it is me, Bane.");
+    System.out.println("What do you want from me?\n");
+    System.out.println("___________________________________________________\n");
+    
+    String input = sc.nextLine();
+    while (!input.equals("bye")) {
+      System.out.println("___________________________________________________\n");
+      System.out.println("\t" + input);
+      System.out.println("___________________________________________________\n");
+      
+      input = sc.nextLine();
+    }
+       
+    System.out.println("___________________________________________________\n");
+    System.out.println("Bye, hope to not see you again.\n");
+    System.out.println("***************************************************\n");
+    sc.close();
     }
 }
