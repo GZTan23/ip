1dbe8e721fab71e9c46de3c5bf4929217212f131
GZTan23 <guan_zhong@GuanZhong.>
2025-01-31 00:58:31 +0800

    Add Ui.java to compile most UI elements into one file

--- src/main/java/Bane.java
+++ src/main/java/Bane.java
@@ -11,142 +11,130 @@ import java.util.Scanner;
 public class Bane {     
 	public static ArrayList<Task> al = new ArrayList<>(); 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		try {
-			loadTasks();
-			greeting(); 
-		
-			String input; 
-			do {  
-			input = sc.nextLine();
-			response(input);
 
-		} while(!input.startsWith("bye"));
-		
+		try (Scanner sc = new Scanner(System.in)) {
+			Ui.greeting();
+			loadTasks();
+			
+			String input;
+			do {
+				input = sc.nextLine();
+				response(input);
+				
+			} while(!input.startsWith("bye"));
+			
 			saveTasks();
-		} catch (IOException e) {
-			responseTemplate(e.getMessage());
-			sc.close();
-			System.exit(0);
 		}
+
+
+	
 	}
 	
 	
 	public static void response(String dialogue) {
 
 		if (dialogue.startsWith("bye")) {
-			responseTemplate("Bye, hope to not see you again.\n");
+			Ui.farewell();
 		
 		} else if (dialogue.startsWith("list")) {
-
+			Ui.separatorLine();
+			
 			if (al.isEmpty()) {
-				responseTemplate("what were you expecting? A present? It's empty!");
+				
+				Ui.listReply("empty");
 			} else {
-				StringBuilder sb = new StringBuilder("Reminding you of things you have already forgetten:\n");
+
+				Ui.listReply("success");
 				for (int i = 1; i <= al.size(); i++) {
-					sb.append(String.format("    %d. %s\n", i, al.get(i - 1)));
-				}
-				responseTemplate(sb.toString());
-			
-			
+					System.out.print(String.format("    %d. %s\n", i, al.get(i - 1)));
+				}	
 			}
+
+			Ui.separatorLine();	
 		} else if ((dialogue.startsWith("mark")) |
 				(dialogue.startsWith("unmark"))) {
 			
 			try {
 				String[] arr = dialogue.split(" ");
-				int num = Integer.parseInt(arr[1]);
-				StringBuilder sb;
+				int idx = Integer.parseInt(arr[1]);
+
+				Ui.separatorLine();
 
 				if (arr[0].equals("mark")) {
-					al.get(num - 1).taskStatus(true);
-					sb = new StringBuilder("Finally getting work done eh?\n");
-					
+					al.get(idx - 1).taskStatus(true);
+					Ui.markReply("marked");	
 				} else {
-					al.get(num - 1).taskStatus(false);
-					sb = new StringBuilder("As expected, you didn't do it and tried to cheat.\n");
-
+					al.get(idx - 1).taskStatus(false);
+					Ui.markReply("unmarked");
 				}
 
-				sb.append("    " + num + "." + al.get(num - 1));
-				responseTemplate(sb.toString());
+				System.out.println("    " + idx + "." + al.get(idx - 1));
+				Ui.separatorLine();
+
 			} catch (IndexOutOfBoundsException e) {
-				responseTemplate(e.toString() + """
-						You're trying to unmark/mark something that doesn't exist!
-						Format: unmark/mark [task index]""");
+				Ui.separatorLine();
+				System.out.println("Mark Non-Existent Entry.\n");
+				Ui.markReply("index_out_of_bounds");
+				Ui.separatorLine();
+
 			}
 
 		} else if ((dialogue.startsWith("todo")) ||
 				(dialogue.startsWith("deadline")) ||
 				(dialogue.startsWith("event"))) {
-					try {  
-						executeTasks(dialogue);
-					} catch (TaskExecuteException e) {
-						responseTemplate(e.toString() + "\nWow, you're bad at this. Try again.");
-					}
-		}else if (dialogue.startsWith("delete")) {
+				Ui.separatorLine();
+					
+				try {  
+					executeTasks(dialogue);
+
+				} catch (TaskExecuteException e) {
+					System.out.println(e.getMessage());
+					Ui.taskReply("fail");
+
+				}
+				
+				Ui.separatorLine();
+
+		} else if (dialogue.startsWith("delete")) {
+			Ui.separatorLine();
+
 			try {
 				int index = Integer.parseInt(dialogue.split(" ")[1]);
 				al.remove(index-1);
-				responseTemplate("Giving up are we? You disappoint me.");
+				Ui.deleteReply("success");
+
 			} catch (ArrayIndexOutOfBoundsException e) {
-				responseTemplate(e.toString() + """
-						\nFormat: delete [integer]
-						I do not understand how it is so hard to be correct.""");
+				System.out.println("Empty Command.\n");
+				Ui.deleteReply("empty_command");
+				
 			} catch (IndexOutOfBoundsException e) {
-				responseTemplate(e.toString() + "\nYou are trying to delete something that isn't even there.");
+				System.out.println("Delete Non-Existing Entry.\n");
+				Ui.deleteReply("delete_out_of_bounds");
 			} 
+			
+			Ui.separatorLine();
 
 		} else {
-			responseTemplate("I fail to comprehend the inner machinations of the \nthing you call a brain. Try again");
+			Ui.separatorLine();
+			Ui.unknownInputReply();
+			Ui.separatorLine();
 		}
 
 	}
 
-	public static void responseTemplate(String message) {
-		System.out.println("________________________________________________________________\n");
-		System.out.println(message);	
-		System.out.println("________________________________________________________________\n");
-	}
 	
 
-	public static void greeting() {
-		String logo ="\n\t▄▄▄▄    ▄▄▄       ███▄    █ ▓█████  \n"
-					+  "\t▓█████▄ ▒████▄     ██ ▀█   █ ▓█   ▀ \n"
-					+  "\t▒██▒ ▄██▒██  ▀█▄  ▓██  ▀█ ██▒▒███   \n"
-					+  "\t▒██░█▀  ░██▄▄▄▄██ ▓██▒  ▐▌██▒▒▓█  ▄ \n"
-					+  "\t░▓█  ▀█▓ ▓█   ▓██▒▒██░   ▓██░░▒████▒\n"
-					+  "\t░▒▓███▀▒ ▒▒   ▓▒█░░ ▒░   ▒ ▒ ░░ ▒░ ░\n"
-					+  "\t▒░▒   ░   ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ░  ░\n"
-					+  "\t░    ░   ░   ▒      ░   ░ ░    ░    \n"
-					+  "\t░            ░  ░         ░    ░  ░ \n"
-					+  "\t      ░                             \n";
-		System.out.println(logo);
-		responseTemplate("""
-				Hello, it is me, Bane.
-				Why have you called upon me?\n""");
-	}
-
-	public static void replyToTasks(Task t) {
-		StringBuilder sb = new StringBuilder("Added to list of things to \"forget\",\n");
-		sb.append("  " + t);
-		sb.append(String.format("\nwhich makes the total: %d.\n",al.size()));
-		responseTemplate(sb.toString());
-	}
 	
 	public static void executeTasks(String dialogue) throws TaskExecuteException {
 		String[] diagParts = dialogue.split(" ", 2);
 		if (diagParts.length < 2) {
-			responseTemplate("""
-					You have to input something else other than the command itself,
-					just in case you have forgotten. 
-					Format: [command] [task] <duration if applicable> """);
+			Ui.taskReply("empty command");
 		} else {
 			switch (diagParts[0]) {
 			case "todo":
 				ToDo tTask = new ToDo(diagParts[1]);
 				al.add(tTask);
-				replyToTasks(tTask);
+				Ui.taskReply("success", tTask, al.size());
 				break;
 			case "event":
        			try {
@@ -155,23 +143,23 @@ public class Bane {
 					
 					//check if user has entered strictly following the format
 					if (!((taskParts[1].startsWith("from")) && (taskParts[2].startsWith("to")))) {
-						throw new TaskExecuteException("Format: event [task] /from [time] /to [time]");
+						throw new TaskExecuteException("Wrong Format.\n\nFormat: event [task] /from [time] /to [time]");
 					}
        			    String start = taskParts[1].split(" ", 2)[1];
        			    String end =  taskParts[2].split(" ", 2)[1];
 					
 					Event eTask = new Event(taskParts[0], start, end);
 					al.add(eTask);
-					replyToTasks(eTask);
-					} catch (ArrayIndexOutOfBoundsException e) {
-						throw new TaskExecuteException("Format: event [task] /from [time] /to [time]");
+					Ui.taskReply("success", eTask, al.size());
+				} catch (ArrayIndexOutOfBoundsException e) {
+					throw new TaskExecuteException("Wrong Format.\n\nFormat: event [task] /from [time] /to [time]");
 
-					} catch (DateTimeParseException e) {
-						throw new TaskExecuteException(
-								 "\nFormat for time: [DD-MM-YYYY] [HH:mm].\nCan be either or both.");
+				} catch (DateTimeParseException e) {
+					throw new TaskExecuteException(
+							"Wrong Date Format.\n\nFormat for time: [DD-MM-YYYY] [HH:mm].\nCan be date or both.");
 
-					}
-					break;
+				}
+				break;
 			case "deadline":
 				try {
 					String[] taskParts = diagParts[1].split("/");
@@ -179,24 +167,28 @@ public class Bane {
 					
 					Deadline dTask = new Deadline(taskParts[0], deadline);
 					al.add(dTask);
-					replyToTasks(dTask);
+					Ui.taskReply("success", dTask, al.size());
 					
 				} catch (ArrayIndexOutOfBoundsException e) {
-					throw new TaskExecuteException(e.toString() + "\nFormat: deadline [task] /by [deadline]");
+					throw new TaskExecuteException("Wrong Format.\n\nFormat: deadline [task] /by [deadline]");
 
 				} catch (DateTimeParseException e) {
-					throw new TaskExecuteException(e.toString()+ "\nFormat for time: [DD-MM-YYYY] [HH:mm].\n Can be either or or both.");
+					throw new TaskExecuteException(
+						"Wrong Date Format.\n\nFormat for time: [DD-MM-YYYY] [HH:mm].\nCan be date or both.");
 				}
 				break;
+			default:
+				throw new TaskExecuteException("Unknown Command.\n");
 			}
 		}
 	}
 
-	public static void saveTasks() throws IOException {	
+	public static void saveTasks() {	
 		DateTimeFormatter saver = DateTimeFormat.SAVE_FORMAT.formatter();
-
+		BufferedWriter bw;
 		try {
-			BufferedWriter bw = Files.newBufferedWriter(Paths.get("./data/Bane.txt"));
+			bw = Files.newBufferedWriter(Paths.get("./data/Bane.txt"));
+
 			for (Task task : al) {
 				String input = "";
 				String taskStatus = (task.getTaskStatus()) ? "1" : "0";
@@ -215,75 +207,89 @@ public class Bane {
 					bw.write(input, 0, input.length());
 					bw.newLine();
 				} catch (IOException e) {
-					bw.close();
-					throw new IOException(e.toString() + """
-							\nLooks like there was an error whilst saving
-							Heh! I'll leave you to handle it :P""");
+					Ui.separatorLine();
+					System.out.println("File Write Error.\n");
+					Ui.saveReply("write_error");
+					Ui.separatorLine();
+
+					System.exit(1);
 				}
 			}
 			bw.close();
-        } catch (IOException e) {
-			throw new IOException(e.toString() + """
-					\nLooks like your files aren't working like they used to.	""");
-			
-        }
-		
 
+		} catch (IOException e) {
+			Ui.separatorLine();
+			System.out.println("File Open Error.\n");
+			Ui.saveReply("file_open_error");
+			Ui.separatorLine();
+
+			System.exit(1);
+		}
 	}
 
 	//TODO: Add checks for corruption and invalid inputs
 
-	public static void loadTasks() throws IOException {
-		//if file/directory does not exist
-		if (Files.notExists(Paths.get("./data/Bane.txt"))) {
-			try {
+	public static void loadTasks() {
+		try {
+			//if file/directory does not exist
+			if (Files.notExists(Paths.get("./data/Bane.txt"))) {
 				if (Files.notExists(Paths.get("./data"))) {
 					Files.createDirectory(Paths.get("./data"));
 				}
 				Files.createFile(Paths.get("./data/Bane.txt"));
-				responseTemplate("""
-						Added new file "./data/Bane.txt" because I clearly have
-						to do everything for you.""");
-			} catch (IOException e) {
-				throw new IOException(e.toString() + """
-						\nIt seems that you're on your own.
-						Create "./data/Bane.txt" and wake me up when done.""");
+				Ui.loadReply("success");
+
 			}
-		}
-		try {
-			BufferedReader br = Files.newBufferedReader(Paths.get("./data/Bane.txt"));
-			String line = br.readLine();
-		
-			//check whether there is still more in the file
-			while (line != null) {
-				String[] lineParts = line.split(",");
-				boolean isDone = lineParts[1].trim().equals("1");
-				switch (lineParts[0]) {
+			try {
+				BufferedReader br = Files.newBufferedReader(Paths.get("./data/Bane.txt"));
+				String line = br.readLine();
+			
+				//check whether there is still more in the file
+				while (line != null) {
+					String[] lineParts = line.split(",");
+					if (lineParts.length < 3) {
+						throw new IOException();
+					}
 
-				case "T":
-					ToDo tTask = new ToDo(lineParts[2]);
-					tTask.taskStatus(isDone);
-					al.add(tTask);					
-					break;
+					boolean isDone = lineParts[1].trim().equals("1");
+					switch (lineParts[0]) {
 
-				case "D":
-					Deadline dTask = new Deadline(lineParts[2], lineParts[3]);
-					dTask.taskStatus(isDone);
-					al.add(dTask);
-					break;
+					case "T":
+						ToDo tTask = new ToDo(lineParts[2]);
+						tTask.taskStatus(isDone);
+						al.add(tTask);					
+						break;
 
-				case "E":
-					Event eTask = new Event(lineParts[2], lineParts[3], lineParts[4]);
-					eTask.taskStatus(isDone);
-					al.add(eTask);
-					break;
+					case "D":
+						Deadline dTask = new Deadline(lineParts[2], lineParts[3]);
+						dTask.taskStatus(isDone);
+						al.add(dTask);
+						break;
+
+					case "E":
+						Event eTask = new Event(lineParts[2], lineParts[3], lineParts[4]);
+						eTask.taskStatus(isDone);
+						al.add(eTask);
+						break;
+					}
+					line = br.readLine();
 				}
-				line = br.readLine();
+			} catch (IOException e) {
+				Ui.separatorLine();
+				System.out.println("Read File Error\n");
+				Ui.loadReply("read_file_error");
+				Ui.separatorLine();
+
+				System.exit(1);
 			}
 		} catch (IOException e) {
-			throw new IOException(e.toString() + """
-					\nGahhhh! Something went wrong with the file!
-					Fix it and get back to me dirtbag!""");
-		}		
+			Ui.separatorLine();
+			System.out.println("File Creation Fail\n");
+			Ui.loadReply("file_creation_fail");
+			Ui.separatorLine();
+
+			System.exit(1);
+
+		}
 	}
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,154 @@
+public class Ui {
+
+    public static void separatorLine() {
+	    System.out.println("________________________________________________________________\n");
+    }
+
+	public static void greeting() {
+  
+		String logo ="\n\t▄▄▄▄    ▄▄▄       ███▄    █ ▓█████  \n"
+					+  "\t▓█████▄ ▒████▄     ██ ▀█   █ ▓█   ▀ \n"
+					+  "\t▒██▒ ▄██▒██  ▀█▄  ▓██  ▀█ ██▒▒███   \n"
+					+  "\t▒██░█▀  ░██▄▄▄▄██ ▓██▒  ▐▌██▒▒▓█  ▄ \n"
+					+  "\t░▓█  ▀█▓ ▓█   ▓██▒▒██░   ▓██░░▒████▒\n"
+					+  "\t░▒▓███▀▒ ▒▒   ▓▒█░░ ▒░   ▒ ▒ ░░ ▒░ ░\n"
+					+  "\t▒░▒   ░   ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ░  ░\n"
+					+  "\t░    ░   ░   ▒      ░   ░ ░    ░    \n"
+					+  "\t░            ░  ░         ░    ░  ░ \n"
+					+  "\t      ░                             \n";
+
+		System.out.println(logo); 
+        separatorLine();
+		System.out.println("""
+				Hello, it is me, Bane.
+				Why have you called upon me?\n""");
+        separatorLine();
+	}
+
+    public static void farewell() {
+        separatorLine();
+		System.out.println("Bye, hope to not see you again.");
+        separatorLine();
+    }
+
+    public static void listReply(String type) {
+        switch (type) {
+        case "empty":
+            System.out.println("What were you expecting? A present? It's empty!");
+            break;
+
+        case "success":
+            System.out.println("Reminding you of things you have already forgetten:\n");
+            break;
+
+        }
+    }
+
+    public static void markReply(String type) {
+
+        switch (type) {
+        case "marked":
+            System.out.println("Finally getting work done eh?\n");
+            break;
+
+        case "unmarked":
+            System.out.println("As expected, you didn't do it and tried to cheat.\n");
+            break;
+
+        case "index_out_of_bounds":
+            System.out.println("""
+						You're trying to unmark/mark something that doesn't exist!
+						Format: unmark/mark [task index]""");
+            break;
+        }
+    }
+
+    public static void taskReply(String type) {
+        switch (type) {
+        case "fail":
+            System.out.println("Wow, you're bad at this. Try again.");
+            break;
+
+        case "empty command":
+            System.out.println("""
+					You have to input something else other than the command itself,
+					just in case you have forgotten. 
+					Format: [command] [task] <duration if applicable> """);
+            break;
+        }
+    }
+
+    public static void taskReply(String type, Task task, int alSize) {
+        switch (type) {
+        case "success":
+            StringBuilder sb = new StringBuilder("Added to list of things to \"forget\",\n");
+            sb.append("  " + task);
+            sb.append(String.format("\nwhich makes the total: %d.\n", alSize));
+            System.out.println(sb.toString());
+            break;
+
+        }
+    }
+
+    public static void deleteReply(String type) {
+        switch (type) {
+        case "success":
+			System.out.println("Giving up are we? You disappoint me.");
+            break;
+
+        case "empty_command":
+            System.out.println("""
+						I do not understand how it is so hard to be correct.
+						Format: delete [integer]""");
+            break;
+
+        case "delete_out_of_bounds":
+            System.out.println("You are trying to delete something that isn't even there.");
+            break;
+        }
+    }
+
+    public static void unknownInputReply() {
+		System.out.println("I fail to comprehend the inner machinations of the \nthing you call a brain. Try again");
+
+    }
+
+    public static void loadReply(String type) {
+        switch (type) {
+        case "success":
+            System.out.println("""
+                    Added new file "./data/Bane.txt" because I 
+                    clearly have to do everything for you.""");
+            break;
+
+        case "file_creation_fail":
+            System.out.println("""
+                    It seems that you're on your own.
+                    Create "./data/Bane.txt" and wake me up when done.""");
+            break;
+
+        case "read_file_error":
+            System.out.println("""
+					Gahhhh! Something went wrong with the file!
+					Fix it and get back to me dirtbag!""");
+            break;
+        }
+    }
+
+    public static void saveReply(String type) {
+        switch (type) {
+        case "write_error":
+            System.out.println("""
+                    Looks like there was an error whilst saving
+                    Heh! I'll leave you to handle it :P"""); 
+                break;
+
+        case "file_open_error":
+            System.out.println("""
+					Looks like your files aren't working like they used to.""");
+            break;
+            
+        }
+    }
+    
+}
