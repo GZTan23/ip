e0427238bff425206856f3704397af5d03d4c18c
GZTan23 <guan_zhong@GuanZhong.>
2025-01-30 15:59:34 +0800

    Add data time functionality

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,16 +1,27 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.TemporalAccessor;
+
 public class Deadline extends Task{
-    private String deadline;
-    public Deadline(String name, String deadline) {
+    private TemporalAccessor deadline;
+    private final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("dd-MM-uuuu[ HH:mm]");
+
+    
+    public Deadline(String name, String deadline) throws DateTimeParseException {
         super(name);
-        this.deadline = deadline;
+        this.deadline = DATE_TIME_FORMAT.parseBest(deadline, LocalDateTime::from,
+                LocalDate::from, LocalTime::from);
     }
 
     public String getDeadline() {
-        return this.deadline;
+        return DateTimeFormatter.ofPattern("MMM d yyyy[ HH:mm]").format(deadline);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + String.format(" (by: %s)", this.deadline);
+        return "[D]" + super.toString() + String.format(" (by: %s)", getDeadline());
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,22 +1,32 @@
-public class Event extends Task {
-    private String start, end;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.TemporalAccessor;
 
-    public Event(String name, String start, String end) {
+public class Event extends Task {
+    private TemporalAccessor start, end;
+    private final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("dd-MM-uuuu[ HH:mm]");
+
+    public Event(String name, String start, String end) throws DateTimeParseException {
         super(name);
-        this.start = start;
-        this.end = end;
+        this.start = DATE_TIME_FORMAT.parseBest(start, LocalDateTime::from,
+                LocalDate::from, LocalTime::from);
+        this.end = DATE_TIME_FORMAT.parseBest(end, LocalDateTime::from,
+                LocalDate::from, LocalTime::from);
     } 
 
     public String getStart() {
-        return this.start;
+        return DateTimeFormatter.ofPattern("MMM d yyyy[ HH:mm]").format(start);
     }
 
     public String getEnd() {
-        return this.end;
+        return DateTimeFormatter.ofPattern("MMM d yyyy[ HH:mm]").format(end);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + String.format(" (from: %s to: %s)", start, end);
+        return "[E]" + super.toString() + String.format(" (from: %s to: %s)", getStart(), getEnd());
     }
 }
